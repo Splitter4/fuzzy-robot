c1b75cbabaa3c65136cceffa00193fd2502d91ab
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2016-04-17 23:51:40 -0300

    Regras e defuzzificação com arrays de conjuntos

--- main.cpp
+++ main.cpp
@@ -7,36 +7,58 @@
 #include <math.h>
 
 #define TAMANHO_CONJUNTO 360 + 1 // De -180 até 180, incluindo o 0.
+#define CONJUNTOS_BOLA 3 // Esquerda, frente e direita.
+#define CONJUNTOS_ALVO 3 // Esquerda, frente e direita.
 
 // Conjuntos relativos à posição da bola.
+/*
 float Eb[TAMANHO_CONJUNTO];
 float Fb[TAMANHO_CONJUNTO];
 float Db[TAMANHO_CONJUNTO];
+*/
+float conjBola[CONJUNTOS_BOLA][TAMANHO_CONJUNTO];
 
 // Conjuntos relativos à posição do alvo.
+/*
 float Ea[TAMANHO_CONJUNTO];
 float Fa[TAMANHO_CONJUNTO];
 float Da[TAMANHO_CONJUNTO];
+*/
+float conjAlvo[CONJUNTOS_ALVO][TAMANHO_CONJUNTO];
 
 // Conjuntos relativos à posição de saida.
+/*
 float Er[TAMANHO_CONJUNTO];
 float Fr[TAMANHO_CONJUNTO];
 float Dr[TAMANHO_CONJUNTO];
+*/
+float conjSaida[CONJUNTOS_BOLA][CONJUNTOS_ALVO][TAMANHO_CONJUNTO];
 
 // Graus de pertinência dos conjuntos de entrada.
+/*
 float bolaDireita;
 float bolaFrente;
 float bolaEsquerda;
+*/
+float uBola[CONJUNTOS_BOLA];
+
+/*
 float alvoDireita;
 float alvoFrente;
 float alvoEsquerda;
+*/
+float uAlvo[CONJUNTOS_ALVO];
 
+/*
 float acaoFrente[3];
 float acaoEsquerda[3];
 float acaoDireita[3];
+*/
 
+/*
 float acao;
 int tipoAcao;
+*/
 
 float triangulo(float x, float alfa, float beta, float gama)
 {
@@ -68,19 +90,25 @@ void init()
 {
     //int variacao = 360/TAMANHO_CONJUNTO;
     
-    for (int x = 0; x <= TAMANHO_CONJUNTO; x++)
+    for (int i = 0; i <= TAMANHO_CONJUNTO; i++)
     {
-        Eb[x] = triangulo(x - 180,  -190,   -180,   0   );
-        Fb[x] = triangulo(x - 180,  -90 ,    0  ,   90  );
-        Db[x] = triangulo(x - 180,  -0  ,    180,   190 );
-
-        Ea[x] = triangulo(x - 180,  -190,   -180,   0   );
-        Fa[x] = triangulo(x - 180,  -90 ,    0  ,   90  );
-        Da[x] = triangulo(x - 180,  -0  ,    180,   190 );
-
-        Er[x] = triangulo(x - 180,  -190,   -180,      0);
-        Fr[x] = triangulo(x - 180,  -180,      0,    180);
-        Dr[x] = triangulo(x - 180,    -0,    180,    190);
+        conjBola[0][i] = triangulo(i - 180,  -190,   -180,   0   ); // Esquerda.
+        conjBola[1][i] = triangulo(i - 180,  -90 ,    0  ,   90  ); // Frente.
+        conjBola[2][i] = triangulo(i - 180,  -0  ,    180,   190 ); // Direita.
+        
+        conjAlvo[0][i] = triangulo(i - 180,  -190,   -180,   0   ); // Esquerda.
+        conjAlvo[1][i] = triangulo(i - 180,  -90 ,    0  ,   90  ); // Frente.
+        conjAlvo[2][i] = triangulo(i - 180,  -0  ,    180,   190 ); // Direita.
+        
+        conjSaida[0][0][i] = triangulo(i - 180,  -180,      0,    180); // Frente.
+        conjSaida[0][1][i] = triangulo(i - 180,  -190,   -180,      0); // Esquerda.
+        conjSaida[0][2][i] = triangulo(i - 180,  -190,   -180,      0); // Esquerda.
+        conjSaida[1][0][i] = triangulo(i - 180,    -0,    180,    190); // Direita.
+        conjSaida[1][1][i] = triangulo(i - 180,  -180,      0,    180); // Frente.
+        conjSaida[1][2][i] = triangulo(i - 180,  -190,   -180,      0); // Esquerda.
+        conjSaida[2][0][i] = triangulo(i - 180,    -0,    180,    190); // Direita.
+        conjSaida[2][1][i] = triangulo(i - 180,    -0,    180,    190); // Direita.
+        conjSaida[2][2][i] = triangulo(i - 180,  -180,      0,    180); // Frente.
 
         //theta = theta + variacao;
     }
@@ -93,6 +121,7 @@ int emGraus(float radianos)
 
 void fuzzyficacao(float ballAngle, float targetAngle)
 {
+    /*
     bolaDireita  = Db[emGraus(ballAngle) + 180];
     bolaFrente   = Fb[emGraus(ballAngle) + 180];
     bolaEsquerda = Eb[emGraus(ballAngle) + 180];
@@ -100,179 +129,83 @@ void fuzzyficacao(float ballAngle, float targetAngle)
     alvoDireita  = Da[emGraus(targetAngle) + 180];
     alvoFrente   = Fa[emGraus(targetAngle) + 180];
     alvoEsquerda = Ea[emGraus(targetAngle) + 180];
+    */
+    
+    for(int i = 0; i < 3; i++)
+    {
+        uBola[i] = conjBola[i][emGraus(ballAngle) + 180];
+        uAlvo[i] = conjAlvo[i][emGraus(targetAngle) + 180];
+    }
 }
 
-
-void regras()
+float min(float a, float b)
 {
-    for (int i = 0; i < 3; i++) // Inicialização das Variaveis
+    if(a <= b)
+        return a;
+    else
+        return b;
+}
+
+float max(float a, float b)
+{
+    if(a >= b)
+        return a;
+    else
+        return b;
+}
+
+void regras(float *conjFinal)
+{
+    for(int i = 0; i < CONJUNTOS_BOLA; i++)
     {
-        acaoFrente[i] = 0;
-        acaoEsquerda[i] = 0;
-        acaoDireita[i] = 0;
+        for(int j = 0; j < CONJUNTOS_ALVO; j++)
+        {
+            if(uBola[i] > 0 && uAlvo[j] > 0) // Regra ativada.
+            {
+                // Obtém a força de disparo da regra
+                // pela conjunção dos graus de pertinência.
+                float alfa = min(uBola[i], uAlvo[j]);
+                
+                // Corta o conjunto de saída da regra
+                // e faz a união com os outros conjuntos cortados
+                for(int k = 0; k < TAMANHO_CONJUNTO; k++)
+                    conjFinal[k] = max(min(alfa, conjSaida[i][j][k]), conjFinal[k]);
+            }
+        }
+    }
+}
+
+float defuzzificacao(float *conjFinal)
+{
+    float dividendo = 0;
+    float divisor = 0;
+    
+    for(int i = 0; i < TAMANHO_CONJUNTO; i++)
+    {
+        dividendo += conjFinal[i]*(i - 180);
+        divisor += conjFinal[i];
     }
     
-    
-    if (bolaEsquerda && alvoEsquerda) // Regra 01
-    {
-        if(bolaEsquerda > alvoEsquerda)
-            acaoFrente[0] = alvoEsquerda;
-        else
-            acaoFrente[0] = bolaEsquerda;
-    }
-    if (bolaEsquerda && alvoFrente) // Regra 02
-    {
-        if(bolaEsquerda > alvoFrente)
-            acaoEsquerda[0] = alvoFrente;
-        else
-            acaoEsquerda[0] = bolaEsquerda;
-    }
-    if (bolaEsquerda && alvoDireita) // Regra 03
-    {
-        if(bolaEsquerda > alvoDireita)
-            acaoEsquerda[1] = alvoDireita;
-        else
-            acaoEsquerda[1] = bolaEsquerda;
-    }
-    if (bolaFrente   && alvoEsquerda) // Regra 04
-    {
-        if(bolaFrente > alvoEsquerda)
-            acaoDireita[0] = alvoEsquerda;
-        else
-            acaoDireita[0] = bolaFrente;
-    }
-    if (bolaFrente   && alvoFrente) // Regra 05
-    {
-        if(bolaFrente > alvoFrente)
-            acaoFrente[1] = alvoFrente;
-        else
-            acaoFrente[1] = bolaFrente;
-    }
-    if (bolaFrente   && alvoDireita) // Regra 06
-    {
-        if(bolaFrente > alvoDireita)
-            acaoEsquerda[2] = alvoDireita;
-        else
-            acaoEsquerda[2] = bolaFrente;
-    }
-    if (bolaDireita  && alvoEsquerda) // Regra 07
-    {
-        if(bolaDireita > alvoEsquerda)
-            acaoDireita[1] = alvoEsquerda;
-        else
-            acaoDireita[1] = bolaDireita;
-    }
-    if (bolaDireita  && alvoFrente) // Regra 08
-    {
-        if(bolaDireita > alvoFrente)
-            acaoDireita[2] = alvoFrente;
-        else
-            acaoDireita[2] = bolaDireita;
-    }
-    if (bolaDireita  && alvoDireita) // Regra 09
-    {
-        if(bolaDireita > alvoDireita)
-            acaoFrente[2] = alvoDireita;
-        else
-            acaoFrente[2] = bolaDireita;
-    }
+    return dividendo/divisor;
 }
 
-void defuzzificacao()
+int main( int argc, char* argv[] )
 {
-    int i, j;
-    acao = 0;
-    float maxAcaoEsquerda = 0;
-    float maxAcaoFrente = 0;
-    float maxAcaoDireita = 0;
-
-    // Conjuntos relativos à posição de saida.
-    float Es[TAMANHO_CONJUNTO];
-    float Fs[TAMANHO_CONJUNTO];
-    float Ds[TAMANHO_CONJUNTO];
-    float aux;
-    int Econt=0,Fcont=0,Dcont=0;
-
-    for (i = 0; i<3; i++)
-    {
-        if (acaoEsquerda[i]>maxAcaoEsquerda)
-        {
-            maxAcaoEsquerda = acaoEsquerda[i]; 
-        }
-        if (acaoFrente[i]>maxAcaoFrente)
-        {
-            maxAcaoFrente = acaoFrente[i];
-        }
-        if (acaoDireita[i]>maxAcaoDireita)
-        {
-            maxAcaoDireita = acaoDireita[i];
-        }
-    }
-
-    for (i = 0; i <TAMANHO_CONJUNTO; i++)
-    {
-        if(Er[i] > maxAcaoEsquerda)
-            Es[i] = maxAcaoEsquerda;
-        else
-            Es[i] = Er[i];
-        if(Fr[i] > maxAcaoFrente)
-            Fs[i] = maxAcaoFrente;
-        else
-            Fs[i] = Fr[i];
-        if(Dr[i] > maxAcaoDireita)
-            Ds[i] = maxAcaoDireita;
-        else
-            Ds[i] = Dr[i]; 
-    }
-
-    j = 0;
-    for (i = -180; i <=180; i++)
-    {
-        if(Es[j] > Fs[j]){
-            if(Es[j] > Ds[j]){
-                aux = Es[j];
-                Econt++;
-            }else{
-                aux = Ds[j];
-                Dcont++;
-            }
-        }else{
-            if(Fs[j] > Ds[j]){
-                aux = Fs[j];
-                Fcont++;
-            }else{
-                aux = Ds[j];
-                Dcont++;
-            }
-
-        }
-
-        acao = acao + (i * aux); 
-    }
-
-    acao = acao / (Econt * maxAcaoEsquerda + Fcont * maxAcaoFrente + Dcont * maxAcaoDireita);
-}
-
-int main( int argc, char* argv[] ) {
-
-    float ballAngle, targetAngle, leftMotor, rightMotor;
-
-    // Declaração do objeto que representa o ambiente.
-    environm::soccer::clientEnvironm environment;
-
     if ( argc != 3 )
     {
         printf( "Invalid parameters. Expecting:\n" );
-        printf( "SoccerPlayer SERVER_ADDRESS_STRING SERVER_PORT_NUMBER\n" );
-        printf( "\nExample:\n" );
+        printf( "SoccerPlayer SERVER_ADDRESS_STRING SERVER_PORT_NUMBER\n\n" );
+        printf( "Example:\n" );
         printf( "SoccerPlayer localhost 1024\n\n" );  
               
         return 0;
     }
 
-    // Conecta-se ao SoccerMatch. Supõe que SoccerMatch está rodando na máquina
-    // local e que um dos robôs esteja na porta 1024. Porta e local podem mudar.
-    if ( ! environment.connect( argv[1], atoi( argv[2] ) ) )
+    // Declaração do objeto que representa o ambiente.
+    environm::soccer::clientEnvironm environment;
+
+    // Conecta-se ao SoccerMatch.
+    if ( environment.connect( argv[1], atoi(argv[2]) ) == false)
     {
         printf( "Fail connecting to the SoccerMatch.\n\n" );
         
@@ -285,34 +218,37 @@ int main( int argc, char* argv[] ) {
     // Inicializa conjuntos.
     init();
 
-    while ( 1 ) {
-
-        // Deve obter os dados desejados do ambiente. Métodos do clientEnvironm.
-        // Exemplos de métodos que podem ser utilizados.
+    float ballAngle, targetAngle, leftMotor, rightMotor;
+    
+    float conjFinal[TAMANHO_CONJUNTO];
+    
+    while ( true )
+    {
         ballAngle = environment.getBallAngle();
         targetAngle = environment.getTargetAngle( environment.getRivalGoal() );
         
         // Obtém graus de pertinência.
-        fuzzyficacao(ballAngle,targetAngle);
+        fuzzyficacao(ballAngle, targetAngle);
 
-        // Corta os conjuntos e faz a união deles
-        regras();
+        // Corta os conjuntos de saída das regras ativadas e faz a união deles
+        for(int i = 0; i < TAMANHO_CONJUNTO; i++)
+            conjFinal[i] = 0; // A união de 0 com qualquer coisa é qualquer coisa
+        
+        regras(conjFinal);
 
         // Obtém saída final
-        defuzzificacao();
+        float resultado = defuzzificacao(conjFinal);
 
-        leftMotor  = acao;
-        rightMotor = 1 - acao;
+        leftMotor  = (resultado/(-360) + 0.5)*0.1;
+        rightMotor = (resultado/360 + 0.5)*0.1;
 
-        leftMotor *= 0.9;
-        rightMotor *= 0.9;
-
+        //leftMotor *= 0.9;
+        //rightMotor *= 0.9;
         
         // Transmite ação do robô ao ambiente. Fica bloqueado até que todos os
         // robôs joguem. Se erro, retorna false (neste exemplo, sai do laco).
-        if ( ! environment.act( leftMotor, rightMotor ) ) {
+        if ( environment.act( leftMotor, rightMotor ) == false )
             break; // Termina a execução se falha ao agir.
-        }
     }
 
     return 0;
