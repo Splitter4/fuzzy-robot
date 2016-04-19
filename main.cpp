4739c15e2cf07365d15941d4247493c853b19688
Artur de Almeida Scheibler <artur.scheibler@gmail.com>
2016-04-19 00:43:26 -0300

    Implementação atual 100% correta

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// Artur de Almeida Scheibler
+// Julio Pinto
 //------------------------------------------------------------------------------
 // Inclusão da biblioteca que implementa a interface com o SoccerMatch.
 #include "environm.h"
@@ -6,59 +8,23 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define TAMANHO_CONJUNTO 360 + 1 // De -180 até 180, incluindo o 0.
+#define TAMANHO_CONJUNTO 360 // De -180 até 180.
 #define CONJUNTOS_BOLA 3 // Esquerda, frente e direita.
 #define CONJUNTOS_ALVO 3 // Esquerda, frente e direita.
 
 // Conjuntos relativos à posição da bola.
-/*
-float Eb[TAMANHO_CONJUNTO];
-float Fb[TAMANHO_CONJUNTO];
-float Db[TAMANHO_CONJUNTO];
-*/
 float conjBola[CONJUNTOS_BOLA][TAMANHO_CONJUNTO];
 
 // Conjuntos relativos à posição do alvo.
-/*
-float Ea[TAMANHO_CONJUNTO];
-float Fa[TAMANHO_CONJUNTO];
-float Da[TAMANHO_CONJUNTO];
-*/
 float conjAlvo[CONJUNTOS_ALVO][TAMANHO_CONJUNTO];
 
 // Conjuntos relativos à posição de saida.
-/*
-float Er[TAMANHO_CONJUNTO];
-float Fr[TAMANHO_CONJUNTO];
-float Dr[TAMANHO_CONJUNTO];
-*/
 float conjSaida[CONJUNTOS_BOLA][CONJUNTOS_ALVO][TAMANHO_CONJUNTO];
 
 // Graus de pertinência dos conjuntos de entrada.
-/*
-float bolaDireita;
-float bolaFrente;
-float bolaEsquerda;
-*/
 float uBola[CONJUNTOS_BOLA];
-
-/*
-float alvoDireita;
-float alvoFrente;
-float alvoEsquerda;
-*/
 float uAlvo[CONJUNTOS_ALVO];
 
-/*
-float acaoFrente[3];
-float acaoEsquerda[3];
-float acaoDireita[3];
-*/
-
-/*
-float acao;
-int tipoAcao;
-*/
 
 float triangulo(float x, float alfa, float beta, float gama)
 {
@@ -66,24 +32,19 @@ float triangulo(float x, float alfa, float beta, float gama)
     
     if ((x <= alfa) || (x >= gama)) 
     {
-        mi=0;
-        return mi;
+        mi = 0;
     }
-    
-    if ((x >= alfa) && (x <= beta)) 
+    else if ((x > alfa) && (x <= beta)) 
     {
         mi = (x - alfa)/(beta - alfa);
-        return mi;
     }
-    
-    if ((x >  beta) && (x <  gama)) 
+    else if ((x > beta) && (x < gama)) 
     {
-        mi=(gama - x)/(gama - beta);
-        return mi;
+        mi = (gama - x)/(gama - beta);
     }
     
     return mi;
-} 
+}
 
 // Inicialização de conjuntos.
 void init()
@@ -94,20 +55,22 @@ void init()
     {
         conjBola[0][i] = triangulo(i - 180,  -190,   -180,   0   ); // Esquerda.
         conjBola[1][i] = triangulo(i - 180,  -90 ,    0  ,   90  ); // Frente.
-        conjBola[2][i] = triangulo(i - 180,  -0  ,    180,   190 ); // Direita.
+        conjBola[2][i] = triangulo(i - 180,   0  ,    180,   190 ); // Direita.
         
         conjAlvo[0][i] = triangulo(i - 180,  -190,   -180,   0   ); // Esquerda.
         conjAlvo[1][i] = triangulo(i - 180,  -90 ,    0  ,   90  ); // Frente.
-        conjAlvo[2][i] = triangulo(i - 180,  -0  ,    180,   190 ); // Direita.
+        conjAlvo[2][i] = triangulo(i - 180,   0  ,    180,   190 ); // Direita.
         
         conjSaida[0][0][i] = triangulo(i - 180,   -90,      0,     90); // Frente.
         conjSaida[0][1][i] = triangulo(i - 180,  -190,   -180,      0); // Esquerda.
         conjSaida[0][2][i] = triangulo(i - 180,  -190,   -180,      0); // Esquerda.
-        conjSaida[1][0][i] = triangulo(i - 180,    -0,    180,    190); // Direita.
+        
+        conjSaida[1][0][i] = triangulo(i - 180,     0,    180,    190); // Direita.
         conjSaida[1][1][i] = triangulo(i - 180,   -90,      0,     90); // Frente.
         conjSaida[1][2][i] = triangulo(i - 180,  -190,   -180,      0); // Esquerda.
-        conjSaida[2][0][i] = triangulo(i - 180,    -0,    180,    190); // Direita.
-        conjSaida[2][1][i] = triangulo(i - 180,    -0,    180,    190); // Direita.
+        
+        conjSaida[2][0][i] = triangulo(i - 180,     0,    180,    190); // Direita.
+        conjSaida[2][1][i] = triangulo(i - 180,     0,    180,    190); // Direita.
         conjSaida[2][2][i] = triangulo(i - 180,   -90,      0,     90); // Frente.
 
         //theta = theta + variacao;
@@ -121,16 +84,6 @@ int emGraus(float radianos)
 
 void fuzzyficacao(float ballAngle, float targetAngle)
 {
-    /*
-    bolaDireita  = Db[emGraus(ballAngle) + 180];
-    bolaFrente   = Fb[emGraus(ballAngle) + 180];
-    bolaEsquerda = Eb[emGraus(ballAngle) + 180];
-    
-    alvoDireita  = Da[emGraus(targetAngle) + 180];
-    alvoFrente   = Fa[emGraus(targetAngle) + 180];
-    alvoEsquerda = Ea[emGraus(targetAngle) + 180];
-    */
-    
     for(int i = 0; i < 3; i++)
     {
         uBola[i] = conjBola[i][emGraus(ballAngle) + 180];
@@ -222,7 +175,7 @@ int main( int argc, char* argv[] )
     
     float conjFinal[TAMANHO_CONJUNTO];
     
-    int k = 15;
+    int k = 25;
     
     while ( true )
     {
@@ -244,7 +197,7 @@ int main( int argc, char* argv[] )
         leftMotor = (resultado/360 + 0.5)*0.1;
         rightMotor  = (resultado/(-360) + 0.5)*0.1;
         
-        if(++k >= 15)
+        if(++k >= 25)
         {
             printf("ballAngle = %d | targetAngle = %d\n", emGraus(ballAngle), emGraus(targetAngle));
             printf("resultado = %f\n", resultado);
